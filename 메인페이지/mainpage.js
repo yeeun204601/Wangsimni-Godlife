54566538738b38b421cd0debdb3ecd06a7788521
Son Yeeun <yeeun204601@naver.com>
2023-05-21 21:31:49 +0900

    메인 페이지 자바스크립트- 오늘의 한 마디 수정

--- 메인페이지/mainpage.js
+++ 메인페이지/mainpage.js
@@ -22,7 +22,7 @@ let comments = [
     '아자아자, 파이팅이닷!', '수고했어, 오늘도', '잘해왔고, 잘하고 있어!', '울지말고 웃자!', '행복하자ㅎㅎ',
     '언제나 응원해~', '좋은 일이 가득하길!', '할 수 있어!', '행복 가득, 행운 가득~', '덤벼라! 세상아'
 ]
-let todays_num = Math.floor((Math.random() * comments.length)) + 1;
+let todays_num = Math.floor((Math.random() * comments.length));
 
 document.getElementById("comment").innerHTML = "오늘의 한 마디: " + comments[todays_num];
 
