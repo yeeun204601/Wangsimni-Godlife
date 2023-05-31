df448a17c421b33273b8a6630d56e7c9581fc24a
Kimchi-in-hole <62549934+Kimchi-in-hole@users.noreply.github.com>
2023-05-31 16:01:32 +0900

    Update homeTraining_timer.js

--- 메인페이지/건강한나/homeTraining_timer.js
+++ 메인페이지/건강한나/homeTraining_timer.js
@@ -1,6 +1,5 @@
-var minutes = prompt("분을 입력하세요", "5");
-var seconds = prompt("초를 입력하세요", "0");
-
+var minutes = prompt("시간(분)을 입력하세요", "5");
+var seconds = 0;
 
 var timeElement = document.getElementById("timer");
 var startButton = document.getElementById("startButton");
@@ -10,16 +9,16 @@ var totalTime = minutes * 60 + seconds;
 updateTimer();
 
 function updateTimer() {
- 
-  var minutesRemaining = Math.floor((totalTime % 3600) / 60);
+  var minutesRemaining = Math.floor(totalTime / 60);
   var secondsRemaining = totalTime % 60;
 
-  var formattedTime = ("00") + ":" + ("0" + minutesRemaining).slice(-2) + ":" + ("0" + secondsRemaining).slice(-2);
+  var formattedTime =
+    ("00" + minutesRemaining).slice(-2) +
+    ":" +
+    ("00" + secondsRemaining).slice(-2);
 
-  
   timeElement.textContent = formattedTime;
 
- 
   if (totalTime > 0) {
     totalTime--;
   } else {
@@ -27,17 +26,12 @@ function updateTimer() {
   }
 }
 
-
-startButton.addEventListener("click", function() {
-  
+startButton.addEventListener("click", function () {
   timerInterval = setInterval(updateTimer, 1000);
-
   startButton.disabled = true;
 });
 
-
-pauseButton.addEventListener("click", function() {
+pauseButton.addEventListener("click", function () {
   clearInterval(timerInterval);
-
   startButton.disabled = false;
 });
